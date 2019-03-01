e3812b2339493a66a2333ccc04b602d5957b34cf
Alex Angel <alexiosangeletakis@yahoo.com>
2019-02-28 20:21:09 -0800

    Update Number2Letter.cpp

--- Games/Number2Letter.cpp
+++ Games/Number2Letter.cpp
@@ -2,16 +2,16 @@
 
 int main() {
   int letter = 0;
-  std::cout << "Please type in a number and a letter will be printed on screen corresponding to it's value in the English Alphabet:\n\n";
+  std::cout << "Please type in a number: ";
 
   while (1) {
     std::cin >> letter;
     //less than 1 or greater than 26
-    if (letter < 1 || 26 < letter) {
-      std::cout <<"The English Alphabet only has 26 letters.\nTry again\n";
-    } else {
-        std::cout << "\nThe letter that corosponds to that value is '" << char(64+letter) << "'\n";
+    if (!(letter < 1 || 26 < letter)) {
+        std::cout << "The letter that corosponds to that value is '" << char(64+letter) << "'\n";
       break;
     }
+    std::cout <<"The English Alphabet only has 26 letters.\nTry again\n";
   }
+  
 }
