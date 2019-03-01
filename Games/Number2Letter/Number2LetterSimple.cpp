b8a9884aecc19234d6e766109fa8ae12cc26d3e1
Alex Angel <alexiosangeletakis@yahoo.com>
2019-03-01 06:19:45 -0800

    Update Number2LetterSimple.cpp

--- Games/Number2Letter/Number2LetterSimple.cpp
+++ Games/Number2Letter/Number2LetterSimple.cpp
@@ -8,7 +8,7 @@ int main() {
     std::cin >> letter;
     //less than 1 or greater than 26
     if (!(letter < 1 || 26 < letter)) {
-        std::cout << "The letter that corosponds to that value is '" << char(64+letter) << "'\n";
+        std::cout << "The letter that corosponds to that value is '" << char('A' + (letter - 1)) << "'\n";
       return 0;
     }
     std::cout <<"The English Alphabet only has 26 letters.\nTry again\n";
